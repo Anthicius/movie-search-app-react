4559139daa8946db0d1ae2423c1b330cb9d5b7d0
Anthicius <andrius.makseckas@gmail.com>
2026-01-11 16:36:49 +0200

    added useEffect for fetching movies from API. Added Movie Search component to be able to search specific movies from API. Haven't finished error handling and movie detail rendering fully.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,45 @@
+import { useEffect, useState } from "react";
+import MovieSearch from "./Components/MovieSearch";
 
 function App() {
+  const [movieData, setMovieData] = useState(null);
+  const [movieTitleSearch, setMovieTitleSearch] = useState("");
+  const [error, setError] = useState(false)
 
-  return (
-    <>
-     <h1>hello</h1>
-    </>
-  )
+  const handleSearch = (searchTerm) => {
+    if(searchTerm.trim().length > 0){
+    setMovieTitleSearch(searchTerm);
+    }
+  }
+
+  useEffect(() => {
+    const load = async () => {
+      try {
+        if(movieTitleSearch.length === 0){
+          return;
+        }
+        const response = await fetch(
+          `https://www.omdbapi.com/?t=${movieTitleSearch}&apikey=d74835ef`
+        );
+        const data = await response.json();
+        if(data.Response === "False"){
+          return setError(true)
+        } else{
+          setMovieData(data);
+          setError(false)
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    load();
+  }, [movieTitleSearch]);
+
+  return <>
+  <MovieSearch onSearch={handleSearch}/>
+  {!error && <h1>Movie not found</h1>}
+  {movieData && <h1>{movieData.Title}</h1>}
+  </>;
 }
 
-export default App
+export default App;
---
+++ src/Components/MovieSearch.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+
+const MovieSearch = ({onSearch}) => {
+    const [searchTerm, setSearchTerm] = useState("")
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        onSearch(searchTerm)
+    }
+
+  return (
+    <form onSubmit={handleSubmit}>
+        <input
+            minLength={1}
+            value={searchTerm}
+            type='text'
+            onChange={(e)=>setSearchTerm(e.target.value)}
+            placeholder='Movie title...'
+        />
+        <button type='submit'>Search</button>
+    </form>
+  )
+}
+
+export default MovieSearch
